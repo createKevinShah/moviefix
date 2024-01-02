214efd4be0f8b9420ffa91e531e939b7bf3a735b
kevin <kevin@antarcticaglobal.com>
2024-01-02 10:14:21 +0530

    fix:bugs

--- src/Components/CustomCard.jsx
+++ src/Components/CustomCard.jsx
@@ -1,30 +1,38 @@
 import React from "react";
 import { motion } from "framer-motion";
+import MoviePlaceholder from "../Assets/Icons/MoviePlaceholder";
 
 const CustomCard = ({ title, image, ratings, onClick = () => {} }) => {
   return (
-    <div>
-      <motion.div
-        whileHover={{
-          scale: 1.03,
-          transition: { duration: 0.2 },
-        }}
-        whileTap={{ scale: 0.9 }}
-        className="bg-white w-[90vw] md:w-[330px] h-[500px] md:h-[490px] rounded-2xl overflow-hidden relative"
+    <motion.div
+      whileHover={{
+        scale: 1.03,
+        transition: { duration: 0.2 },
+      }}
+      whileTap={{ scale: 0.9 }}
+      role="button"
+      onClick={onClick}
+    >
+      <div
+        className="bg-black w-[90vw] md:w-[330px] h-[500px] md:h-[490px] rounded-2xl overflow-hidden relative"
         style={{
           boxShadow: "4px 4px 8px -2px rgba(16, 24, 40, 0.08)",
         }}
-        role="button"
-        onClick={onClick}
       >
         <div className="w-full h-full">
-          <img
-            src={`${process.env.REACT_APP_POSTER_URL}${image}`}
-            alt={title}
-            className="w-full h--full object-cover"
-          />
+          {image ? (
+            <img
+              src={`${process.env.REACT_APP_POSTER_URL}${image}`}
+              alt={title}
+              className="object-cover w-full h--full"
+            />
+          ) : (
+            <div className="flex items-center justify-center h-full">
+              <MoviePlaceholder className="text-grey-10" />
+            </div>
+          )}
         </div>
-      </motion.div>
+      </div>
       <div className="mt-3 w-[90vw] md:w-[330px]">
         <div className="">
           <p className="text-white font-semibold text-base max-w-[90%] truncate">
@@ -32,13 +40,13 @@ const CustomCard = ({ title, image, ratings, onClick = () => {} }) => {
           </p>
           <div className="flex items-center gap-x-2">
             <img src="/ratings.svg" className="w-5 h-5" alt="ratings" />
-            <p className="text-white font-medium text-base">
+            <p className="text-base font-medium text-white">
               {ratings.toFixed(1)}
             </p>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,25 +1,22 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="fixed top-0 left-0 right-0 h-[50px] bg-black z-20">
-      <div className="primary-layout bg-black">
-        <div
-          role="button"
-          className="flex items-center justify-start w-[162px]"
-          onClick={() => navigate("/")}
-        >
-          <p className="text-red-100 font-semibold text-3xl pl-4 py-4">
-            Moviefix
-          </p>
-          <img
-            src="/moviefix-logo.png"
-            className="w-[90px] h-[90px] ml-[-10px]"
-            alt="Moviefix"
-          />
+      <div className="bg-black primary-layout">
+        <div className="w-[190px] ml-4">
+          <a href="/">
+            <div className="flex items-center justify-center w-full ml-[12px]">
+              <p className="py-4 text-3xl font-semibold text-red-100">
+                Moviefix
+              </p>
+              <img
+                src="/moviefix-logo.png"
+                className="w-[90px] h-[90px]"
+                alt="Moviefix"
+              />
+            </div>
+          </a>
         </div>
       </div>
     </div>
